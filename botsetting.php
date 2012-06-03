a80de42c8c8ede86c895804207e3927c0f07af88
Wayne Lai <abev66@gmail.com>
2012-06-03 16:50:01 +0800

    UI: redesign botsetting.php

--- botsetting.php
+++ botsetting.php
@@ -1,10 +1,7 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php //session check
 session_start();
-if(!isset($_SESSION['uid'])){
-	header("location: index.php");
-	}
-	if($_SESSION['type']!='a'){
+if(!isset($_SESSION['uid']) || $_SESSION['type']!='a'){
 	header("location: index.php");
 	}
 else{
@@ -13,7 +10,6 @@ else{
 	$session_uid=$_SESSION["uid"];
 	$session_name=$_SESSION["name"];
 	$session_type=$_SESSION["type"];
-	}
 ?>
 <html>
 <head>
@@ -23,7 +19,7 @@ else{
 </head>
 <body>
 	<div class='status'>
-		<?php echo "HI! ".$session_name.", <a href='passwd.php'>change password<a>, <a href='logout.php'>logout</a>"?>
+		<?php echo "HI! ".$session_name.", <a href='passwd.php'>change password</a>, <a href='logout.php'>logout</a>"?>
 	</div>
 	<div class='banner'>
 	<img src="http://i.imgur.com/wvLcp.png" alt="設定" width="500" height="100" />
@@ -35,34 +31,71 @@ else{
 			echo "<li><a class='links' href='botcontrol.php'>robot control</a></li>";
 			}
 		if($session_uid==1){
-			echo "<li/><a class='links' href='accountmanage.php'>account manage</a><li>";
+			echo "<li><a class='links' href='accountmanage.php'>account manage</a><li>";
 			}
 		?>
 		</ul>
-	</div>
 	</div>
 	<div class='botlink'><ul><li>
 	<a class='links' href='botcontrol.php'>botcontrol</a></li><li>
 	<a class='links' href='botsetting.php'>botsetting</a></li></ul>
 	</div>
 	<div class='content'><br />
-	<?php
+<?php if(!isset($_POST['update'])): ?>
+      <form action='' method='POST'>
+	<table border='1' id='fortable'>
+	  <thead>
+	    <tr>
+	      <th>SETTING</th>
+	      <th>VALUE</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+<?php
 	$settings=dump_settings($dblink);
-	echo "<table border='1' id='fortable' align='center'><th>SETTING NAME</th><th>VALUES</th><th />";
-	foreach($settings as $k=>$i){
-			echo "<tr><td>".$k."</td><td>".$i."</td><td><form action='' method='POST'><input type='hidden' name='updateset' value='".$k."' />";
-			echo "<input type='hidden' name='updatevalue' value='".$i."'><input type='submit' name='update' value='修改'></form></td></tr>";
-		}
-	echo "</table>";
-	if (isset($_POST['update'])){
-	echo "<form action='' method='POST'><input type='hidden' name='updatesetc' value='".$_POST['updateset']."'><input type='text' name='updatevaluec' value='".$_POST['updatevalue']."'><input type='submit' value='修改'></form>";
-	}
-	if (isset($_POST['updatesetc'])){
-		if (!update_setting($dblink, $_POST['updatesetc'], $_POST['updatevaluec']))
-			echo "update sucess!!";
-		}
-	?>
+	
+	foreach( $settings as $key => $value ){
+	  echo "<tr>\n";
+	  echo "<td>$key</td>\n";
+	  echo '<td>';
+	  if($key == 'RESPONSE_MODE'){
+	      foreach(array('MENTION','ANYWAY','DISABLED') as $set)
+		echo "<input type='radio' name='newsets[$key]' value='$set' ".(($set == strtoupper($value))?'checked':'')." />$set";
+	  } else if(strtolower($value) == 'true' || strtolower($value) == 'false') {
+	      if(strtolower($value)=='true'){
+		echo "<input type='radio' name='newsets[$key]' value='true' checked /> On";
+		echo "<input type='radio' name='newsets[$key]' value='false' /> Off";
+	      } else {
+		echo "<input type='radio' name='newsets[$key]' value='true' /> On";
+		echo "<input type='radio' name='newsets[$key]' value='false' checked /> Off";
+	      }
+	  } else
+	    echo "<input type='text' name='newsets[$key]' value='$value' />";
+	    
+	  echo "<input type='hidden' name='oldsets[$key]' value='$value' />";
+	  echo '</td>';
+	  echo "\n</tr>\n";
+}
+?>
+	  </tbody>
+	</table>
+	<input type='submit' name='update' value='儲存設定' />
+	<input type='button' value='取消修改' onclick='javascript: location.href="botsetting.php";' />
+    </form>
+<?php
+ else:
+    
+    foreach($_POST['newsets'] as $key => $value) {
+      if( $value != $_POST['oldsets'][$key] ){
+	update_setting($dblink, $key, $value);
+      }
+    }
+    header('Location: botsetting.php');
+ endif;
+?>
 	</div>
-	<?php db_close($dblink);?>
+<?php
+} 
+db_close($dblink);?>
 </body>
 </html>
