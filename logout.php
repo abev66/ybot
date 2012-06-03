39d5a46cda684b84ffee3577af7bd6b37a104f69
Wayne Lai <abev66@gmail.com>
2012-06-03 12:31:55 +0800

    redesigned logout.php

--- logout.php
+++ logout.php
@@ -1,21 +1,32 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<html>
-<?php/*
-session_start();
-if(!isset($_SESSION['uid'])){
-	header("location: index.php");
-	}
-else{
-	session_destroy();
-	}*/
+﻿<?php
+  session_start();
+  
+  if(!isset($_SESSION['uid']))
+    header("location: index.php");
+  else {
+    unset($_SESSION["uid"], $_SESSION["name"], $_SESSION["type"]);
 ?>
-<head><title>see ya!</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-<body>
-<div class='logout'>
-logout sucessed!!<br /><a href='login.php'>relogin with another account<a>
-</div>
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+    <title>see ya!</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <style>
+      <!--
+	.container>div {
+/*	  position: fixed;*/
+	  text-align: center;
+	  display:block;
+	  margin: 150px auto 0;
+	}
+      -->
+    </style>
+  </head>
+  <body>
+    <div class='container'>
+      <div>Logout successfully!!<br /> <a href='login.php'>Relogin with another account<a></div>
+    </div>
+  </body>
 </html>
+<?php } ?>
