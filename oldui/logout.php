2ea5c35a67e599353a5c1eb47949f3dd587d113e
lRabbit <abev66@gmail.com>
2012-08-02 05:21:40 +0800

    remove old ui.

--- oldui/logout.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-  session_start();
-  
-  if(isset($_SESSION['_ybot_uid']))
-    unset($_SESSION["_ybot_uid"], $_SESSION["_ybot_name"], $_SESSION["_ybot_type"]); 
-  header('Location: index.php') 
-?>
